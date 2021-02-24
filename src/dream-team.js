b9a99cb426366ccb5dd74aafe3c8812f4c6cad9f
al119c <example@gmail.com>
2021-02-24 21:57:21 +0300

    feat: implement function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function createDreamTeam(members) {
+    let teamNameArr = [];
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+    if (Array.isArray(members)) {
+        for (let i = 0; i < members.length; i++) {
+            if (typeof (members[i]) == 'string') {
+                teamNameArr.push(members[i].trim()[0].toUpperCase());
+            }
+        }
+        return teamNameArr.sort().join('');
+    } else {
+        return false;
+    }
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,23 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function getSeason(date) {
+    if (!date) {
+        return 'Unable to determine the time of year!';
+    }
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+    if (typeof (date) != 'object') {
+        throw 'Error';
+    }
+
+    const monthNum = date.getUTCMonth();
+
+    for (let i = 0; i < 4; i++) {
+        if (monthNum == 0 || monthNum == 1 || monthNum == 11) {
+            return 'winter';
+        } else if (monthNum > 1 && monthNum < 5) {
+            return 'spring';
+        } else if (monthNum > 4 && monthNum < 8) {
+            return 'summer';
+        } else if (monthNum > 7 && monthNum < 11) {
+            return 'autumn';
+        }
+    }
 };
