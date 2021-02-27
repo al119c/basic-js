e39d378ee481378611f49ce9f8a82c4e402643e1
al119c <example@gmail.com>
2021-02-27 22:31:30 +0300

    fix: fix condition

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,9 +4,11 @@ const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(sampleActivity) {
-    if (typeof (sampleActivity) == String) {
+    if (typeof (sampleActivity) === 'string' && !isNaN(sampleActivity) && sampleActivity > 0 && sampleActivity < 15) {
         return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD));
     } else {
         return false;
     }
 };
+
+// console.log(+'0');
