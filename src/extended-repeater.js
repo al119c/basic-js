547ff2305cb60061a3b0a6109c1107241d506968
al119c <example@gmail.com>
2021-03-28 21:25:37 +0300

    feat: implement function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+// function repeater(str, options) {
+  str = str + '';
+  let repeatTimes = (options.repeatTimes || 1),
+      separator = (options.separator || '+'),
+      addition = (options.addition || '') + '',
+      additionSeparator = (options.additionSeparator || '|'),
+      additionRepeatTimes = (options.additionRepeatTimes || 1);
   
+  let arr = [],
+      additionArr = [];
+  
+      for (let i = 0; i < repeatTimes; i++) {
+        arr.push(str);
+    }
+
+    if (addition !== undefined) {
+        for (let i = 0; i < additionRepeatTimes; i++) {
+          additionArr.push(addition);
+        }
+    }
+
+    return arr.map((e) => e + additionArr.join(additionSeparator)).join(separator);
+};
