306008cc8582f34c81200e734600b1d35e662710
al119c <example@gmail.com>
2021-09-05 16:05:03 +0300

    feat: implement extented-repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,27 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  
+  str = str + '';
+  let repeatTimes = (options.repeatTimes || 1),
+      separator = (options.separator || '+'),
+      addition = (options.addition || '') + '',
+      additionSeparator = (options.additionSeparator || '|'),
+      additionRepeatTimes = (options.additionRepeatTimes || 1);
+  
+  let arr = [],
+      additionArr = [];
+  
+      for (let i = 0; i < repeatTimes; i++) {
+        arr.push(str);
+    }
+
+    if (addition) {
+        for (let i = 0; i < additionRepeatTimes; i++) {
+          additionArr.push(addition);
+        }
+    }
+
+    return arr.map((e) => e + additionArr.join(additionSeparator)).join(separator);
 }
