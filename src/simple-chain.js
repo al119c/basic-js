1ceb2147bb0262410591e481b78ff078dbba652c
al119c <example@gmail.com>
2021-09-07 11:41:22 +0300

    feat: implement simple-chain function

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,24 +5,31 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+  chainArr: [],
+
+    getLength() {
+        return this.chainArr.length;
+    },
+    addLink(value) {
+        this.chainArr.push(`( ${value} )`);
+        return this;
+    },
+    removeLink(position) {
+        if (position < 1 || position > this.chainArr.length || !Number.isInteger(position)) {
+            this.chainArr = [];
+            throw new Error("You can't remove incorrect link!");
+        } else {
+            this.chainArr.splice(position - 1, 1);
+            return this;
+        }
+    },
+    reverseChain() {
+        this.chainArr.reverse();
+        return this;
+    },
+    finishChain() {
+        let chainStr = this.chainArr.join('~~');
+        this.chainArr = [];
+        return chainStr;
+    }
 };
